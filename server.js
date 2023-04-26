9f361928e5c7b7545d79bebbc5984c28cc0d503e
AhmedMohammed3 <ahm.moh.has@gmail.com>
2023-04-26 04:00:21 +0200

    Add docker and add connect and disconnect event listeners for sockets

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const socketIO = require('socket.io');
+const http = require('http');
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.set('view engine', 'ejs');
 
@@ -8,10 +13,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.send("Welcome");
+io.on('connection', socket => {
+    console.log("A new user just connected");
+
+    socket.on('disconnect', () => {
+        console.log('A user just disconnected');
+    });
 });
 
-app.listen(PORT, function () {
+server.listen(PORT, function () {
     console.log(`Listening on port ${PORT}!`);
 });
